d33caaa93e66d264f51b8a9fd0cf49920dc934ba
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2023-01-07 16:46:10 +0530

    Time: 153 ms (84.45%), Space: 18.8 MB (26.81%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,6 +1,7 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        intervals = sorted(intervals,key = lambda x:(x[0],x[1]))
+        # intervals = sorted(intervals,key = lambda x:(x[0],x[1]))
+        intervals.sort()
         ans = [intervals[0]]
 
         for start,end in intervals:
