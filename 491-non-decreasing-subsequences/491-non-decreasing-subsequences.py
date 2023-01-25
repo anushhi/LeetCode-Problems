4b4424694946119e62216e1b099bdcb1396a594d
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2023-01-25 18:29:08 +0530

    Time: 240 ms (65.20%), Space: 22.7 MB (27.61%) - LeetHub

--- 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
+++ 491-non-decreasing-subsequences/491-non-decreasing-subsequences.py
@@ -1,23 +1,24 @@
 class Solution:
     def findSubsequences(self, nums: List[int]) -> List[List[int]]:
-        result = set()
-        sequence = []
-
-        def backtrack(index):
-            # if we have checked all elements
-            if index == len(nums):
-                if len(sequence) >= 2:
-                    result.add(tuple(sequence))
+        ans = set()
+        current_seq = []
+        def backtrack(ind):
+            
+            if ind == len(nums):
+                if len(current_seq) >= 2:
+                    ans.add(tuple(current_seq))
                 return
-            # if the sequence remains increasing after appending nums[index]
-            if not sequence or sequence[-1] <= nums[index]:
-                # append nums[index] to the sequence
-                sequence.append(nums[index])
-                # call recursively
-                backtrack(index + 1)
-                # delete nums[index] from the end of the sequence
-                sequence.pop()
-            # call recursively not appending an element
-            backtrack(index + 1)
+            
+            if not current_seq or current_seq[-1] <= nums[ind]:
+                current_seq.append(nums[ind])
+                backtrack(ind + 1)
+                current_seq.pop()
+            backtrack(ind+1)
+            
         backtrack(0)
-        return result
+        return ans 
+            
+            
+            
+                    
+        
