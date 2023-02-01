ae38252939eca161653e7ca736c9075d1fe94843
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2023-02-01 22:47:47 +0530

    Time: 35 ms (70.52%), Space: 13.9 MB (70.81%) - LeetHub

---
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -0,0 +1,14 @@
+from math import gcd
+class Solution:
+    def gcdOfStrings(self, str1: str, str2: str) -> str:
+        if (str1 + str2) != (str2 + str1):
+            return ""
+        if str1 == str2:
+            return str2
+        else:
+            hcf =  gcd(len(str1),len(str2))
+            ans = str1[:hcf]
+            return ans
+            
+        
+        
