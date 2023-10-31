e2f4f095139d0e7bbd08c21b038f82b8712751fc
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2023-10-31 15:02:30 +0530

    Time: 78 ms (27.90%), Space: 16.3 MB (75.51%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.py
@@ -1,9 +1,25 @@
 class Solution:
     
     #Approach :- 1
-        #Hamming Weight usning built in method
+        #Hamming Weight using built in method
         
+    # def sortByBits(self, arr: List[int]) -> List[int]:
+    #     arr.sort(key= lambda num: (num.bit_count(),num))
+    #     return arr
+        
+    #Approach :- 2
+        #Hamming Weight Bit Manipulation
     def sortByBits(self, arr: List[int]) -> List[int]:
-        arr.sort(key= lambda num: (num.bit_count(),num))
-        return arr
+        def findWeight(num):
+            mask = 1
+            weight = 0
+            while num:
+                if num & mask:
+                    weight += 1
+                    num ^= mask
+                    
+                mask <<= 1
+            return weight
         
+        arr.sort(key= lambda num: (findWeight(num),num))
+        return arr                
