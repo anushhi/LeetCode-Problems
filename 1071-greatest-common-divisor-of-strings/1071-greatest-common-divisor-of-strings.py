13215ae1aea78cc31be0d7554ed146058f78df94
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2023-11-01 21:23:41 +0530

    Time: 44 ms (32.31%), Space: 16 MB (96.95%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.py
@@ -4,9 +4,9 @@ class Solution:
         if (str1 + str2) != (str2 + str1):
             return ""
         if str1 == str2:
-            return str2
+            return str1
         else:
-            hcf =  gcd(len(str1),len(str2))
+            hcf = gcd(len(str1),len(str2))
             ans = str1[:hcf]
             return ans
             
