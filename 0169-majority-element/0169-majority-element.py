58bc109b001d218f26b68f6b02b3e63e2bb9098a
Anushka Sharma <65493890+anushhi@users.noreply.github.com>
2024-02-12 05:56:42 +0530

    Time: 135 ms (65.27%), Space: 18 MB (83.33%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.py
@@ -0,0 +1,21 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        # Using Hashmap {TC => O(NLogN),SC=>O(N)}
+        # d = {}
+        # for num in nums:
+        #     d[num] = d.get(num,0)+1
+        #     if d[num] > len(nums)//2:
+        #         return num
+        # Using Moore's Voting Algorithm {TC => O(N),SC => O(1)}
+        count = 0
+        ele = 0
+        for num in nums:
+            if count == 0:
+                ele = num
+            if ele == num:
+                count += 1
+            if ele != num:
+                count -= 1
+        return ele
+        
+            
